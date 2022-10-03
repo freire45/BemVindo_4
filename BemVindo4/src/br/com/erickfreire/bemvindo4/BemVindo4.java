055aee79d96c70a43264fa1269b2272d0c8cc408
Erick Freire <erilefr@gmail.com>
2022-10-03 19:49:20 -0300

    a
    a

---
+++ BemVindo4/src/br/com/erickfreire/bemvindo4/BemVindo4.java
@@ -0,0 +1,16 @@
+package br.com.erickfreire.bemvindo4;
+
+/**
+ * Bem Vindo a Programação em Java! - Versão 4
+ * @author FREIR
+ *
+ */
+
+public class BemVindo4 {
+
+	public static void main(String[] args) {
+		System.out.printf("%s%n%s%n", "Bem vindo a ", "Programação em Java!");
+
+	}
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # BemVindo_4
  Bem Vindo a Programação em Java - Versao 4
+
+## Image
+![Currículo no Computador](bemvindo_4.png)
